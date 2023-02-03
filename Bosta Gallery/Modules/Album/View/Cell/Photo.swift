86502afdc2afa3d6b59176d70d8ce41fe4eafaad
sala7khaled <sala7khaled7@gmail.com>
2023-02-03 06:55:33 +0200

    first commit

---
+++ Bosta Gallery/Modules/Album/View/Cell/Photo.swift
@@ -0,0 +1,21 @@
+//
+//  PhotoCell.swift
+//  Bosta Gallery
+//
+//  Created by Salah Khaled on 03/02/2023.
+//  Copyright © 2023 Salah Khaled. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UICollectionViewCell {
+
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    func configure(with Title: String) {
+        
+    }
+}
